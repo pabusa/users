d14a4c704c3e3228b3e0f4580cf2b73e765adff0
Pablo <pabusa@gmail.com>
2019-11-23 22:41:20 +0100

    Initial commit

---
+++ src/main/java/com/pbs/users/UsersApplication.java
@@ -0,0 +1,13 @@
+package com.pbs.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+
+}
